c37f6ff43e71483e6fa4f80943513e83d7f2805b
unknown <braydarak@gmail.com>
2022-04-27 23:21:06 -0300

    commit 5

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { get } from "../utils/HttpClient";
 import { MovieCard } from "./MovieCard";
 import "./Movies.scss";
